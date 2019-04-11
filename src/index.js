79c741f4f926ef996c4fc7d80a184324e5fdcfe3
Jamesmoo <jamesmoo2090@gmail.com>
2019-04-11 11:18:39 -0600

    test pull
    test pull

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function App() {
 <>
   <h1>Organization Web</h1>
   <h2>Hi tom TAIWAN NUMBA 3</h2>
-  <h3>this is a test with MA own sandbox</h3>
+  <h3>this is a test with MA own sasdasdasdandbox</h3>
 </>;
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
