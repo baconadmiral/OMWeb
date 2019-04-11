80ce3a400a0f9c93b7fe4d683f28547877396835
Anthony.Sparrow <anthony.sparrow@us.af.mil>
2019-04-11 13:27:58 -0600

    From ME2017

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
-  return <div className="App" />;
-}
-<>
-  <h1>Organization Web</h1>
-  <h2>Hi tom TAIWAN NUMBA 3</h2>
-  <h3>this is a test with MA own sandbox</h3>
-</>;
+  return (
+		  <div className="App">
+			  <h1>Organization Web</h1>
+			  <h2>Hi tom TAIWAN NUMBA 666</h2>
+			  <h3>this is a test with MA own sandbox</h3>
+		  </div>
+  		);
+  }
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
