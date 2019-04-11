69a658e37c5ff115eeca49bded1a2b8b52ed1f51
Jamesmoo <jamesmoo2090@gmail.com>
2019-04-11 10:57:12 -0600

    test
    rfrf

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Organization Web</h1>
-      <h2>Hi tom TAIWAN NUMBA 2</h2>
-    </div>
-  );
+  return <div className="App" />;
 }
-
+<>
+  <h1>Organization Web</h1>
+  <h2>Hi tom TAIWAN NUMBA 2 - THIS IS A TEST</h2>
+</>;
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
